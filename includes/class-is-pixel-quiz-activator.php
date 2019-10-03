2ff436911039d0b14729c942882f1a1455404fa0
Nick Temple <nick@nicktemple.com>
2019-10-03 00:05:50 -0400

    initial commit

---
+++ includes/class-is-pixel-quiz-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://intellispire.com
+ * @since      1.0.0
+ *
+ * @package    isPixelQuiz
+ * @subpackage isPixelQuiz/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    isPixelQuiz
+ * @subpackage isPixelQuiz/includes
+ * @author     Intellispire <nick@intellispire.com>
+ */
+class isPixelQuiz_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-is-pixel-quiz-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://intellispire.com
+ * @since      1.0.0
+ *
+ * @package    isPixelQuiz
+ * @subpackage isPixelQuiz/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    isPixelQuiz
+ * @subpackage isPixelQuiz/includes
+ * @author     Intellispire <nick@intellispire.com>
+ */
+class isPixelQuiz_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
